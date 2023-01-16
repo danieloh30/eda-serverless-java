50bb52972ed8cb115968a883e512ac8769765938
Daniel Oh <doh@redhat.com>
2023-01-16 13:53:25 -0500

    Add Quarkus label, delete Singleton, update Quarkus version

--- quarkus-eda-knative-demo/src/main/java/org/acme/EventConsumer.java
+++ quarkus-eda-knative-demo/src/main/java/org/acme/EventConsumer.java
@@ -2,12 +2,10 @@ package org.acme;
 
 import org.jboss.logging.Logger;
 
-import javax.inject.Singleton;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 @Path("/")
-@Singleton
 public class EventConsumer {
 
   private final Logger logger = Logger.getLogger(EventConsumer.class);
